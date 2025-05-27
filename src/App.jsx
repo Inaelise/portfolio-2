35f59d8c944ef81c6841d223480be582766f2d1f
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-27 12:33:50 +0200

    Creates Project component and adds as route

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import PageNotFound from "./components/PageNotFound";
+import Project from "./components/Project";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="project/:id" element={<Project />} />
         <Route path="contact" element={<Contact />} />
         <Route path="*" element={<PageNotFound />} />
       </Route>

3f7b5a767e5b93e085cba6d6446f07b2d7a0ffab
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-27 12:44:40 +0200

    Adds meta description, title and main elements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <h1>This is the home page</h1>;
+  return (
+    <>
+      <meta
+        name="description"
+        content="Welcome to my portfolio. CodeByIna - I can make your visions come true."
+      />
+      <title>CodeByIna</title>
+      <main>
+        <h1>CodeByIna</h1>
+      </main>
+    </>
+  );
 }

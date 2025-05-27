3b330c0a03c39104ce3e6f77865bce21f728c4fe
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-27 12:22:56 +0200

    Creates Home component and adds as route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Layout from "./components/Layout";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }

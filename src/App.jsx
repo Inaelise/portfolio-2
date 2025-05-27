af4115ca5ce9d99a66ce2dfda291e29bc298f5d3
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-27 13:41:32 +0200

    Removes route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Layout from "./components/Layout";
-import Home from "./components/Home";
-import Contact from "./components/Contact";
 import PageNotFound from "./components/PageNotFound";
 import Project from "./components/Project";
 
@@ -10,9 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
         <Route path="project/:id" element={<Project />} />
-        <Route path="contact" element={<Contact />} />
         <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>

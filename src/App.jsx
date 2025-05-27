e16d7addd181230681f4add33585de6e7a679394
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-27 12:29:09 +0200

    Creates Contact component and adds as route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Route, Routes } from "react-router";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
   );

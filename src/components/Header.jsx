ffd8274fb98c80c7446ecbf633d83a32908b5d1d
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-27 13:40:26 +0200

    Adds navbar with links to sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,19 @@
 export default function Header() {
-  return <h1>This is the header</h1>;
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }

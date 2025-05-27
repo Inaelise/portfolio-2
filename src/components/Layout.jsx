41af9ac0f2d46238246eda5cc5e6d7ac854f623f
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-27 13:39:28 +0200

    Adds sections for components

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,24 @@
-import { Outlet } from "react-router";
 import Header from "./Header";
 import Footer from "./Footer";
+import Home from "./Home";
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <main>
+        <section id="home">
+          <Home />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
       <Footer />
     </>
   );

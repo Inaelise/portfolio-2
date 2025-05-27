89c91dd2b0620d25dc3543fb9f56a6c78fc770b6
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-27 13:39:53 +0200

    Removes meta description and title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,7 @@
 export default function Home() {
   return (
     <>
-      <meta
-        name="description"
-        content="Welcome to my portfolio. CodeByIna - I can make your visions come true."
-      />
-      <title>CodeByIna</title>
-      <main>
-        <h1>CodeByIna</h1>
-      </main>
+      <h1>CodeByIna</h1>
     </>
   );
 }

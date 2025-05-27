264ed1117151c0990fa32c06294fc132a3bc10d0
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-27 14:28:12 +0200

    Adds some info and links to github/linkedin

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,26 @@
 export default function Home() {
   return (
     <>
-      <h1>CodeByIna</h1>
+      <div>
+        <h1>CodeByIna</h1>
+        <p>Welcome to my portfolio</p>
+      </div>
+      <div>
+        <h2>Who am I?</h2>
+        <p>
+          I'm Ina and I'm an aspiring frontend developer who's got a higher
+          professional degree in the field. I have a passion for creating
+          beautiful and functional web applications.
+        </p>
+        <div>
+          <a href="https://github.com/Inaelise">
+            <img src="/images/github-mark.png" />
+          </a>
+          <a href="https://www.linkedin.com/in/ina-elise-flom-b55433311/">
+            <span>in</span>
+          </a>
+        </div>
+      </div>
     </>
   );
 }

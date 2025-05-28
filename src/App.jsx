b6669c131f50761564e5cff35ca11525458e66f6
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-28 18:02:01 +0200

    Wraps routes in AnimatePresence

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import { Route, Routes } from "react-router";
 import Layout from "./components/Layout";
 import PageNotFound from "./components/PageNotFound";
 import Project from "./components/Project";
+import { useLocation } from "react-router";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="project/:id" element={<Project />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Route>
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Layout />}>
+          <Route path="/project/:id" element={<Project />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </AnimatePresence>
   );
 }
 

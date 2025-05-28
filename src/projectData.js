8b904eac20e9b091b9eadac22a07c0c37808dd8d
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-28 16:35:26 +0200

    Adds project data object

---
+++ src/projectData.js
@@ -0,0 +1,44 @@
+export const projectData = [
+  {
+    id: 1,
+    name: "Holidaze - Project Exam 2",
+    teaser: "Description teaser here",
+    description: "This is the description",
+    reflection: "My reflections here",
+    image: {
+      src: "/images/holidaze-screenshot.jpg",
+      alt: "Homepage of Holidaze",
+    },
+    links: {
+      live: "https://inaelise-pe2-holidaze.netlify.app/",
+      repo: "https://github.com/Inaelise/FED2-PE2",
+    },
+  },
+  {
+    id: 2,
+    name: "Variety - JavaScript Frameworks",
+    teaser: "Description teaser",
+    description: "Description here",
+    reflection: "My reflections here",
+    image: { src: "/images/variety.webp", alt: "Homepage of Variety" },
+    links: {
+      live: "https://js-frameworks-variety.netlify.app/",
+      repo: "https://github.com/Inaelise/js-frameworks-ca",
+    },
+  },
+  {
+    id: 3,
+    name: "Swift Auction - Semester Project 2",
+    teaser: "Description teaser",
+    description: "Description here",
+    reflection: "My reflections here",
+    image: {
+      src: "/images/swiftauction.webp",
+      alt: "Homepage of Swift Auction",
+    },
+    links: {
+      live: "https://swiftauction.netlify.app/",
+      repo: "https://github.com/Inaelise/Semester-Project-2",
+    },
+  },
+];

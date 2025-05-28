4a3f2c5b5f330e43f215f43bff0db3196edadc6a
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-28 18:03:08 +0200

    Adds Outlet and fix to routing

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,21 +3,31 @@ import Footer from "./Footer";
 import Home from "./Home";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import { Outlet, useLocation } from "react-router";
 
 export default function Layout() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
     <>
       <Header />
       <main>
-        <section id="home">
-          <Home />
-        </section>
-        <section id="projects">
-          <Projects />
-        </section>
-        <section id="contact">
-          <Contact />
-        </section>
+        {isHome ? (
+          <>
+            <section id="home">
+              <Home />
+            </section>
+            <section id="projects">
+              <Projects />
+            </section>
+            <section id="contact">
+              <Contact />
+            </section>
+          </>
+        ) : (
+          <Outlet />
+        )}
       </main>
       <Footer />
     </>

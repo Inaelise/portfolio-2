84fe74e6ff5eb1b4c5405c5c44085969d0f24b4f
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-29 09:23:36 +0200

    Adds effect

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,11 +3,27 @@ import Footer from "./Footer";
 import Home from "./Home";
 import Projects from "./Projects";
 import Contact from "./Contact";
-import { Outlet, useLocation } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export default function Layout() {
   const location = useLocation();
+  const navigate = useNavigate();
   const isHome = location.pathname === "/";
+
+  useEffect(() => {
+    if (isHome && location.state?.scrollTo) {
+      const sectionId = location.state.scrollTo;
+      const section = document.getElementById(sectionId);
+      if (section) {
+        setTimeout(() => {
+          section.scrollIntoView({ behavior: "smooth" });
+
+          navigate(location.pathname, { replace: true, state: {} });
+        }, 100);
+      }
+    }
+  }, [isHome, location, navigate]);
 
   return (
     <>

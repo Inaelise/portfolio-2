83991f77c8af7b61b177bd48f724e4e34dde84b2
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-06-02 16:56:46 +0200

    Changes top position and adds relative to parent

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { MoveRight } from "lucide-react";
 export default function Home() {
   return (
     <>
-      <div className="min-h-110 max-w-[550px] mx-auto mb-6">
+      <div className=" relative min-h-110 max-w-[550px] mx-auto mb-6">
         <div className="relative z-20 flex flex-col gap-1 text-center">
           <h1 className="font-second text-5xl">CodeByIna</h1>
           <p className="text-2xl">Welcome to my portfolio</p>
@@ -11,7 +11,7 @@ export default function Home() {
         <img
           src="/images/b&w-profile.webp"
           alt="Woman smiling"
-          className="w-80 top-36 drop-shadow-md absolute"
+          className="w-80 top-10 drop-shadow-md absolute"
         />
       </div>
       <div className="px-4">

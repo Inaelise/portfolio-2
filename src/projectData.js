fe014b8a2eba1d4795b54c7889507bde93af8128
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-06-03 14:20:20 +0200

    Adds description and teaser info

--- src/projectData.js
+++ src/projectData.js
@@ -4,7 +4,7 @@ export const projectData = [
     name: "Holidaze - Project Exam 2",
     teaser: "A modern front-end accommodation booking application.",
     description:
-      "This is my Project Exam 2 assignment. It's a modern front-end accommodation booking application built with Vite + React and TailwindCSS. All API functionality is managed by Noroff's API Holidaze endpoint. My goal was to make the site easy-to-use and have a modern look with a serene theme. A user can either register as a normal customer or as a venue manager. Customers can view their profile, edit avatar and banner, book venues, view their bookings in their profile, and cancel bookings. Only venue managers can create venues, edit or delete their venues, view bookings of owned venues, and view owned venues in their profile.",
+      "This is my Project Exam 2 assignment - A full-featured front-end booking application built with React + Vite, and TailwindCSS, developed for Noroff's Project Exam 2. The app connects to the Holidaze API to manage user authentication, venue listings, and bookings. Designed with a modern, serene UI focused on clarity and ease of use. This project demonstrates proficiency in React, REST API integration, role-based UI, and building polished, scalable front-end applications.",
     reflection: "My reflections here",
     image: {
       src: "/images/project1.jpg",
@@ -18,8 +18,9 @@ export const projectData = [
   {
     id: 2,
     name: "Variety - JavaScript Frameworks",
-    teaser: "Description teaser",
-    description: "Description here",
+    teaser: "A modern front-end eCom store application.",
+    description:
+      "This is my JavaScript Frameworks course assignment - A modern, responsive front-end eCommerce application built with React + Vite, and CSS Modules. This project interacts with Noroff's API Online Store, handling dynamic product listings, detailed views, a shopping cart, and a contact form with validation. This project demonstrates core front-end development skills, API handling, and UI/UX considerations for real-world applications.",
     reflection: "My reflections here",
     image: { src: "/images/project2.jpg", alt: "Homepage of Variety" },
     links: {
@@ -30,8 +31,9 @@ export const projectData = [
   {
     id: 3,
     name: "Swift Auction - Semester Project 2",
-    teaser: "Description teaser",
-    description: "Description here",
+    teaser: "A modern front-end auction web application.",
+    description:
+      "This is my Semester Project 2 assignment - A front-end auction web application built with Javascript + Vite, and TailwindCSS, developed for Noroff's Semester Project 2. The platform connects to Noroff's auction API, allowing users to register, log in, create listings, and place bids in real time. This project showcases skills in state management, API consumption, conditional rendering, and crafting responsive, user-focused interfaces with modern tools.",
     reflection: "My reflections here",
     image: {
       src: "/images/project3.jpg",

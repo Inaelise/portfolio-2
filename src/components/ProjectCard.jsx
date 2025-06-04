7973fe0bba79c70c0be2feca67e08c4491d4a322
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-06-04 10:24:21 +0200

    Adds new font size on breakpoint

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,8 +13,8 @@ export default function ProjectCard({ id, name, teaser, image }) {
         className="w-full h-full object-cover rounded-lg"
       />
       <div className="sm:absolute sm:inset-0 sm:bg-black sm:opacity-0 group-hover:opacity-90 transition-opacity duration-300 ease-in-out flex flex-col gap-2 py-2 text-white sm:p-4 md:items-center md:justify-center">
-        <h3 className="font-semibold">{name}</h3>
-        <p className="text-sm">{teaser}</p>
+        <h3 className="font-semibold md:text-xl">{name}</h3>
+        <p className="text-sm md:text-base">{teaser}</p>
       </div>
     </Link>
   );

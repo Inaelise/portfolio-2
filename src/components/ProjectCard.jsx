e6fa47fc8ca280c1b26111db51a3fc17a236e2a2
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-06-04 09:46:24 +0200

    Changes to styling classes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,6 +4,7 @@ export default function ProjectCard({ id, name, teaser, image }) {
   return (
     <Link
       to={`/project/${id}`}
+      title="Go to project page"
       className="relative group block w-full overflow-hidden transition-transform duration-300 ease-in-out hover:scale-105 rounded-lg"
     >
       <img
@@ -11,7 +12,7 @@ export default function ProjectCard({ id, name, teaser, image }) {
         alt={image.alt}
         className="w-full h-full object-cover rounded-lg"
       />
-      <div className="sm:absolute sm:inset-0 sm:bg-black sm:opacity-0 group-hover:opacity-90 transition-opacity duration-300 ease-in-out flex flex-col gap-2 py-2 text-white sm:p-4">
+      <div className="sm:absolute sm:inset-0 sm:bg-black sm:opacity-0 group-hover:opacity-90 transition-opacity duration-300 ease-in-out flex flex-col gap-2 py-2 text-white sm:p-4 md:items-center md:justify-center">
         <h3 className="font-semibold">{name}</h3>
         <p className="text-sm">{teaser}</p>
       </div>

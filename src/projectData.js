3299b384bf50a0d9a777c95948088feb5f4cf501
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-06-05 15:38:07 +0200

    Adds reflections

--- src/projectData.js
+++ src/projectData.js
@@ -5,7 +5,8 @@ export const projectData = [
     teaser: "A modern front-end accommodation booking application.",
     description:
       "This is my Project Exam 2 assignment - A full-featured front-end booking application built with React + Vite, and TailwindCSS, developed for Noroff's Project Exam 2. The app connects to the Holidaze API to manage user authentication, venue listings, and bookings. Designed with a modern, serene UI focused on clarity and ease of use. This project demonstrates proficiency in React, REST API integration, role-based UI, and building a polished, scalable front-end application.",
-    reflection: "My reflections here",
+    reflection:
+      "Working on this project has been a valuable learning experience, as it has challenged me to apply key front-end development concepts and best practices that I have been building throughout my studies. This project has helped me strengthen my problem-solving skills and shape-up my understanding of the dev tools I have used. I'm excited to improve it further once I continue growing as a developer.",
     image: {
       src: "/images/project1.jpg",
       alt: "Homepage of Holidaze",
@@ -21,7 +22,8 @@ export const projectData = [
     teaser: "A modern front-end eCom store application.",
     description:
       "This is my JavaScript Frameworks course assignment - A modern, responsive front-end eCommerce application built with React + Vite, and CSS Modules. This project interacts with Noroff's API Online Store, handling dynamic product listings, detailed views, a shopping cart, and a contact form with validation. This project demonstrates core front-end development skills, API handling, and UI/UX considerations for a real-world application.",
-    reflection: "My reflections here",
+    reflection:
+      "When I first created the project, the codebase was functional but lacked comprehensive documentation. the absence of JSDoc comments makes it harder for others to quickly grasp the intent, usage, and expected data of various parts of the application. Therefore I have added documentation as a calculated improvement to enhance the maintainability and readability of the code.",
     image: { src: "/images/project2.jpg", alt: "Homepage of Variety" },
     links: {
       live: "https://js-frameworks-variety.netlify.app/",

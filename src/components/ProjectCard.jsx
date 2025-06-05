bd68b6d37bb069289cecfad6bde74c5dd91c66ad
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-06-05 14:29:01 +0200

    Changes to scale and duration

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,14 +5,14 @@ export default function ProjectCard({ id, name, teaser, image }) {
     <Link
       to={`/project/${id}`}
       title="Go to project page"
-      className="relative group block w-full overflow-hidden transition-transform duration-300 ease-in-out hover:scale-105 rounded-lg"
+      className="relative group block w-full overflow-hidden transition-transform duration-400 ease-in-out hover:scale-103 rounded-lg"
     >
       <img
         src={image.src}
         alt={image.alt}
         className="w-full h-full object-cover rounded-lg"
       />
-      <div className="sm:absolute sm:inset-0 sm:bg-black sm:opacity-0 group-hover:opacity-90 transition-opacity duration-300 ease-in-out flex flex-col gap-2 py-2 text-white sm:p-4 md:items-center md:justify-center">
+      <div className="sm:absolute sm:inset-0 sm:bg-black sm:opacity-0 group-hover:opacity-90 transition-opacity duration-400 ease-in-out flex flex-col gap-2 py-2 text-white sm:p-4 md:items-center md:justify-center">
         <h3 className="font-semibold md:text-xl">{name}</h3>
         <p className="text-sm md:text-base">{teaser}</p>
       </div>
